d8c5497644a9418b3fdfad4f11726ed526b2d8b8
dharmaraj07 <48409162+dharmaraj07@users.noreply.github.com>
2025-03-24 15:25:05 +0530

    five

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',  // Replace with your actual GitHub repo name
+  base: '/Layoutz/',  // Replace with your actual GitHub repo name
 });
