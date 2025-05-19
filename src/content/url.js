dd928eb1309f4a0bc1f3c3c895d3ec2b5a236a5c
dharmaraj07 <48409162+dharmaraj07@users.noreply.github.com>
2025-05-19 14:53:09 +0530

    DIST UPDATE

--- src/content/url.js
+++ src/content/url.js
@@ -1,2 +1,2 @@
-/* export const baseURL = "https://layoutz.in" */
-export const baseURL = "http://localhost:8080"
+export const baseURL = "https://layoutz.in"
+/* export const baseURL = "http://localhost:8080" */
